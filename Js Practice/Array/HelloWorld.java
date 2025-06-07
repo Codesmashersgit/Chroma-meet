3a6a427ae669b8a8adacb41208c1c522df803d3a
Sudhanshu Raj <162010369+Codesmashersgit@users.noreply.github.com>
2025-06-07 21:20:01 +0530

    Delete Js Practice directory

--- Js Practice/Array/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    static {
-        System.out.println("Hello, World!");
-        System.exit(0);  // Optional to ensure the program exits after the static block executes
-    }
-}
